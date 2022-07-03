df22c44de428745f64d18ca6122a3e244778909a
jmayorga94 <javi.mayorga1994@outlook.com>
2022-07-03 15:58:16 -0600

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using sample_app.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace sample_app.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Machine hostMachine = new Machine();
+            hostMachine.Name = Environment.MachineName;
+            hostMachine.OsVersion = Environment.OSVersion.ToString();
+            hostMachine.Processors = Environment.ProcessorCount.ToString();
+
+            return Ok(hostMachine);
+        }
+    }
+}
---
+++ Models/Machine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace sample_app.Models
+{
+    public class Machine
+    {
+        public Machine()
+        {
+        }
+        public String Name { get; set; }
+        public String Processors { get; set; }
+        public String ThreatId { get; set; }
+        public String OsVersion { get; set; }
+    }
+}
